121f4422061d0045c9b14e438b6ce3bfdd733616
Ram Kadambi <ram.kadambi@gmail.com>
2022-08-05 18:46:12 +0530

    Create AddSubFunc.py
    using  2 functions to show addNum and subNum

---
+++ AddSubFunc.py
@@ -0,0 +1,14 @@
+#Unit Test 2 - Write Program to do addition and Subtraction
+# of two numbers using functions
+
+def addNum(x,y):
+  return x+y
+
+def subNum(x,y):
+  retur x-y
+
+firstNum = int(input("Enter first Number : "))
+secondNum = int(input("Enter Second Number : "))
+
+print('adding the numbers : ', addNum(firstNum, secondNum))
+print('subtracting the numbers : ', subNum(firstNum, secondNum))
