cdb25377fa01da5549679c6f682f5ac1746353a4
Ram Kadambi <ram.kadambi@gmail.com>
2022-08-28 19:26:37 +0530

    added the menu for admin and Candidate

--- RNMainmenu.py
+++ RNMainmenu.py
@@ -1,4 +1,38 @@
-print("RN College Main Menu \n")
-print("\n\n Press 1 for Candidate Menu \n")
-print("Press 2 for Adminstration Menu \n")
-print("Press 0 to exit menu \n")
+def showMenu(resMenu):
+	if(resMenu == 1):
+		print("\n\n*********RN College Candidate Menu*********\n \n")
+		print("Press 1 for Create application Registration ")
+		print("Press 2 for View available courses ")
+		print("Press 3 for Viewing the Candidate application ")
+		print("Press 4 for Updating the Candidate application ")
+		print("Press 9 to Go back to main menu ")
+		print("Press 0 to exit menu \n")
+
+
+	else:
+		print("\n\n*********RN College Administration Menu*********\n \n")
+
+		print("Press 1 for Viewing the Candidate application ")
+		print("Press 2 for View available courses ")
+		print("Press 3 for Update the Courses ")
+		print("Press 4 for Updating the Candidate application ")
+		print("Press 9 to Go back to main menu ")
+		print("Press 0 to exit menu \n")		
+	
+	return int(input("Enter Your response - "))
+
+mainResponse = 1
+while(mainResponse != 0):
+	print("\n\n******RN College Main Menu******\n \n")
+	print("Press 1 for Candidate Menu")
+	print("Press 2 for Adminstration Menu")
+	print("Press 0 to exit menu \n")
+
+	mainResponse = int(input("Enter Your response - "))
+	#print("the response ",mainResponse)
+	if(mainResponse < 3):
+		resMenu = showMenu(mainResponse)
+	else:
+		print("******* Please enter a VALID Response *****")
+
+print(" Thanks for using RN College Application - Good Bye ")
